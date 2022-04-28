241c8989ebdb86ace49e9ebf85ce0ed5a55288b3
tourist666 <94537192+tourist666@users.noreply.github.com>
2022-04-28 11:28:32 +0800

    Update intersect.cpp

--- intersect.cpp
+++ intersect.cpp
@@ -3,16 +3,16 @@ vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
 		return intersect(nums2, nums1);
 	}
 	unordered_map <int, int> m;
-	for (int num : nums1) {
-		m[num]++;
+	for (int num : nums1) {  //这里的for结构是用来遍历nums1中的所有数据
+		m[num]++;  //将每个数对应的下标均初始化为1
 	}
 	vector<int> intersection;
 	for (int num : nums2) {
-		if (m.count(num)) {
+		if (m.count(num)) {  //m.count（key）判断key是否在m中，存在返回1，不存在返回0
 			intersection.push_back(num);
 			--m[num];
 			if (m[num] == 0) {
-				m.erase(num);
+				m.erase(num); //擦除数据
 			}
 		}
 	}
